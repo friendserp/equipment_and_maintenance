4ffa5da81e1221014c0c89920658166bc4836351
Sami <101696125+lijsamuael@users.noreply.github.com>
2026-01-01 16:31:50 +0300

    Delete equipment_and_maintenance/equipment_and_maintenance/doctype/sub_category directory

--- equipment_and_maintenance/equipment_and_maintenance/doctype/sub_category/sub_category.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Friends ERP and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Sub Category", {
-// 	refresh(frm) {
-
-// 	},
-// });
