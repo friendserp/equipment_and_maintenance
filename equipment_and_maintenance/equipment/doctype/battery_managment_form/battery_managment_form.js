39d420c17be427b99f9f4faad55ac03ccf2eee12
noly <noly@172.16.16.33>
2026-01-05 12:18:31 +0300

    Add equipment management doctypes (battery management, tyre management, equipment request)

---
+++ equipment_and_maintenance/equipment/doctype/battery_managment_form/battery_managment_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Friends ERP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Battery Managment Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ equipment_and_maintenance/equipment/doctype/tyre_management_form/tyre_management_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Friends ERP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tyre Management Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
