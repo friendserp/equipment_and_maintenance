8b306d9b0e393113e051c1062cbc036717a534d9
lijsamuael <lijsamuael@gmail.com>
2026-05-24 20:16:25 +0000

    changes on may 24 2026

--- equipment_and_maintenance/equipment/doctype/operation_time/operation_time.py
+++ equipment_and_maintenance/equipment/doctype/operation_time/operation_time.py
@@ -1,8 +1,9 @@
-# Copyright (c) 2025, Friends ERP and contributors
+# Copyright (c) 2026, Friends ERP and contributors
 # For license information, please see license.txt
 
+# import frappe
 from frappe.model.document import Document
+
 
 class OperationTime(Document):
 	pass
-
